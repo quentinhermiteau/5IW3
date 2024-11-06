048198a8f1cacf7ceb3ea8a6b54c9afe4741fbc8
Quentin Hermiteau <quentinhermiteau@gmail.com>
2024-11-06 18:56:05 +0100

    correction exercice 11

--- src/app/exercices/11/Slider.jsx
+++ src/app/exercices/11/Slider.jsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { useState } from "react";
-
-export default function Slider({ min, max, onChange }) {
-  const [value, setValue] = useState(1);
-
+export default function Slider({ min, max, value, onChange }) {
   return (
     <div className="range">
       <input
@@ -13,11 +9,7 @@ export default function Slider({ min, max, onChange }) {
         max={max}
         value={value}
         step="1"
-        onChange={(e) => {
-          const value = Number(e.target.value);
-          onChange(value);
-          setValue(value);
-        }}
+        onChange={(e) => onChange(Number(e.target.value))}
       />
       <div>{value}</div>
     </div>
--- src/app/exercices/11/page.jsx
+++ src/app/exercices/11/page.jsx
@@ -1,16 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer } from "react";
 import Slider from "./Slider";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+const initialState = { count: 0, step: 1 };
 
-  const handleIncrement = () => setCount(count + step);
-  const handleDecrement = () => setCount(count - step);
-  const handleReset = () => setCount(0);
-  const handleUpdateStep = (step) => setStep(step);
+const reducer = (state, { action, stepValue }) => {
+  switch (action) {
+    case "increment":
+      // return { count: state.count + state.step, step: state.step };
+      return { ...state, count: state.count + state.step };
+    case "decrement":
+      return { ...state, count: state.count - state.step };
+    case "reset":
+      return initialState;
+    case "updateStep":
+      return { ...state, step: stepValue };
+    default:
+      alert("Unhandled action");
+      break;
+  }
+};
+
+export default function Counter() {
+  const [countStep, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => dispatch({ action: "increment" });
+  const handleDecrement = () => dispatch({ action: "decrement" });
+  const handleReset = () => dispatch({ action: "reset" });
+  const handleUpdateStep = (step) =>
+    dispatch({ action: "updateStep", stepValue: step });
 
   return (
     <main>
@@ -18,13 +37,18 @@ export default function Counter() {
         À l'aide de useEffect et setInterval, faire en sorte que le compteur
         s'incrémente automatiquement toutes les secondes.
       </div>
-      <h1>{count}</h1>
+      <h1>{countStep.count}</h1>
       <div>
         <button onClick={handleDecrement}>-</button>
         <button onClick={handleIncrement}>+</button>
         <button onClick={handleReset}>0</button>
       </div>
-      <Slider min={1} max={10} onChange={handleUpdateStep} />
+      <Slider
+        min={1}
+        max={10}
+        value={countStep.step}
+        onChange={handleUpdateStep}
+      />
     </main>
   );
 }
